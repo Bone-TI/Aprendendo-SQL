fb1148004f5884c5068d6269cf5204dbdf2899e9
Bone-TI <85469429+Bone-TI@users.noreply.github.com>
2021-06-25 01:05:05 -0300

    Add files via upload

---
+++ soluçaoexe.sql
@@ -0,0 +1,41 @@
+--quest1
+create database Matheus;
+
+--quest2
+CREATE TABLE Festival (
+
+Musica int
+
+Estilo varchar(20)
+
+);
+CREATE TABLE  Evento(
+
+dataEvento  varchar(8),
+
+horaEvento  varchar(4),
+
+tipoEvento int 
+
+tipoEvento char
+
+);
+
+--quest3
+ALTER TABLE Evento ADD Usuario VARCHAR(60);
+
+--quest4
+CREATE TABLE Usuario(
+ 
+id inteiro
+ 
+nome texto varchar(60),
+ 
+);
+
+--quest5
+ALTER TABLE Evento DROP CONSTRAINT 
+
+--quest6
+DROP {DATABASE | SCHEMA} [IF EXISTS] 
+db_Matheus
